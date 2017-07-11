7c6087de244dd7f4d60523cfaa49d2b95b922780
Blueibis <jennie.li.2011@gmail.com>
2017-07-11 16:57:14 -0400

    setup ajax request

--- election.js
+++ election.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  var list = document.querySelector('#votes');
   // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(response) {
+    response.candidates.forEach(function(voterInfo) {
+      var voter = document.createElement('li');
+      voter.innerHTML = '<b>Name: ' + voterInfo.name + '</b><br>Votes: ' + voterInfo.votes
+      list.append(voter)
+    });
+  });
 
 });
