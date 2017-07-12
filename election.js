ddea006f7c50fce6827e3111fe93dd15953c38d6
Blueibis <jennie.li.2011@gmail.com>
2017-07-12 17:24:51 -0400

    create submit listener for each submit

--- election.js
+++ election.js
@@ -1,6 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   var list = document.querySelector('#votes');
+
+  function submitVote(e) {
+      e.preventDefault();
+      var idVal = $(this).children('input[type=hidden]').val()
+      console.log(idVal);
+      $.ajax({
+        url: 'https://bb-election-api.herokuapp.com/vote?id=' + idVal,
+        method: 'POST',
+        // data: { id: $(this).children('input[type=hidden]').val() },
+        dataType: 'json'
+      }).done(function(response) {
+        console.log('done!');
+      }).fail(function() {
+        console.log('air roar');
+      });
+  }
+
   // Imagination!
   $.ajax({
     url: 'https://bb-election-api.herokuapp.com/',
@@ -8,10 +25,11 @@ document.addEventListener("DOMContentLoaded", function() {
     dataType: 'json'
   }).done(function(response) {
     response.candidates.forEach(function(voterInfo) {
-      var voter = document.createElement('li');
-      var voteForm = document.createElement('form');
-      var voteButton = document.createElement('input');
+      var voter = document.createElement('li')
       var voteHidden = document.createElement('input')
+      var voteButton = document.createElement('input')
+      var voteForm = document.createElement('form')
+
       voteHidden.setAttribute('type', 'hidden')
       voteHidden.setAttribute('name', 'id')
       voteHidden.setAttribute('value', voterInfo.id)
@@ -22,11 +40,18 @@ document.addEventListener("DOMContentLoaded", function() {
       voteForm.append(voteHidden)
       voteForm.append(voteButton)
 
-
       voter.innerHTML = '<b>Name: ' + voterInfo.name + '</b><br>Votes: ' + voterInfo.votes
       list.append(voter)
       list.append(voteForm)
+
+      var forms = document.querySelectorAll('.vote-form')
+      for(var i = 0; i < forms.length; i++) {
+        forms[i].addEventListener('submit', submitVote);
+      }
     });
   });
-
 });
+// document.addEventListener('loaded', function() {
+//
+//
+// })
