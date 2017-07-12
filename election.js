9ebe67b0363d56c342ad30948a41f9891255e87c
Blueibis <jennie.li.2011@gmail.com>
2017-07-12 16:54:21 -0400

    create vote form/button for each candidate, vote gets 200 ok page

--- election.js
+++ election.js
@@ -9,8 +9,23 @@ document.addEventListener("DOMContentLoaded", function() {
   }).done(function(response) {
     response.candidates.forEach(function(voterInfo) {
       var voter = document.createElement('li');
+      var voteForm = document.createElement('form');
+      var voteButton = document.createElement('input');
+      var voteHidden = document.createElement('input')
+      voteHidden.setAttribute('type', 'hidden')
+      voteHidden.setAttribute('name', 'id')
+      voteHidden.setAttribute('value', voterInfo.id)
+      voteButton.setAttribute('type', 'submit')
+      voteForm.className = 'vote-form';
+      voteForm.setAttribute('method', 'post')
+      voteForm.setAttribute('action', 'https://bb-election-api.herokuapp.com/vote')
+      voteForm.append(voteHidden)
+      voteForm.append(voteButton)
+
+
       voter.innerHTML = '<b>Name: ' + voterInfo.name + '</b><br>Votes: ' + voterInfo.votes
       list.append(voter)
+      list.append(voteForm)
     });
   });
 
